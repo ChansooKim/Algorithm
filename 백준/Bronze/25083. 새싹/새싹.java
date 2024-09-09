a830e66500f97dbd173846e16380eb6d8e70672b
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-09 23:18:30 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |\n");
+        System.out.println(sb);
+    }
+}

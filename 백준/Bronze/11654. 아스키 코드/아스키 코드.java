39f3581d3806ca3bc713903938389b00d1e34f49
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-09 17:37:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char a = (char) br.read();
+        System.out.println(a+0);
+    }
+}

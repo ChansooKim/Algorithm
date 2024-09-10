b1d73e2e44db260a49f65991fdc6002945f4ff44
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-10 17:05:49 +0900

    [Bronze I] Title: 세로읽기, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        char[][] arr = new char[15][15];
+        for(int i=0; i<5; i++) {
+            String input = sc.nextLine();
+            char[] inputArr = input.toCharArray();
+            for(int j=0; j<inputArr.length; j++) {
+                arr[i][j] = inputArr[j];
+            }
+        }
+        for(int i=0; i<15; i++) {
+            for(int j=0; j<15; j++) {
+                if(arr[j][i]!=0) {
+                    System.out.print(arr[j][i]);
+                }
+            }
+        }
+    }
+}

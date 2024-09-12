ab3376ac60d6d43024cbf34245a8daf730126d63
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-12 16:54:15 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 172 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigDecimal n = new BigDecimal(sc.nextLine());
+        System.out.println(n.pow(3));
+        System.out.println(3);
+    }
+}

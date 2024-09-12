ce264cf4ca78f5de4416b6adedc10ab80f3e3791
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-12 16:05:46 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        System.out.println(1);  // 어떤 숫자가 들어와도 O(1)
+        System.out.println(0);
+    }
+}

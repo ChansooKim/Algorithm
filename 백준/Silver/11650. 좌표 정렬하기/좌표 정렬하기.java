c20e2edbf14b8f2b01871af3d7d74b19ee3fcde7
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-15 17:03:11 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1224 ms, Memory: 64420 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for(int i=0; i<n; i++) {
+            String[] input = br.readLine().split(" ");
+            for(int j=0; j<2; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        Arrays.sort(arr, (arr1, arr2)->{
+            if(arr1[0] == arr2[0]) {
+                return arr1[1] - arr2[1];
+            } else {
+                return arr1[0] - arr2[0];
+            }
+        });
+        for(int[] x : arr) System.out.println(x[0]+" "+x[1]);
+        br.close();
+    }
+}

b0c5a44c8a044bc7cf94eebfd2a7e35a2cd633a8
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-09-18 15:32:17 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -3,16 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static int[] fibo = new int[21];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(fibo(n));
+        System.out.println(fibonacci(n));
     }
 
-    static int fibo(int n) {
-        if(n==0) return 0;
-        else if(n==1) return 1;
-        else if(n==2) return 1;
-        else return fibo(n-1)+fibo(n-2);
+    static int fibonacci(int n) {
+        if(n==0) return fibo[n]=0;
+        else if(n==1) return fibo[n]=1;
+        else if(n==2) return fibo[n]=1;
+        else return fibo[n]=fibonacci(n-1)+fibonacci(n-2);
     }
 }

9424d18ecaba816327a97977a470591f1d4ebd23
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-10-02 22:43:30 +0900

    [Bronze III] Title: 접시 안의 원, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double T = Double.parseDouble(br.readLine());
+        System.out.println(Math.round(Math.pow(T/2, 2)));
+    }
+}

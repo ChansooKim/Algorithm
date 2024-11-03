8adc5b2b5843a83fb49932163fa13081fdbfb7f4
KimChansoo <50691187+KimChansoooo@users.noreply.github.com>
2024-11-03 18:22:12 +0900

    [Bronze III] Title: 더하기, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            int N = sc.nextInt();
+            int sum = 0;
+            for(int j=0; j<N; j++) {
+                sum += sc.nextInt();
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
